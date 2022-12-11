790110ace790151f04c6230bb9fead97b32299d8
Anton Spiridonov <hoorbazoom@gmail.com>
2022-12-11 17:06:14 +0700

    Была добавлена возможность перестановки букв в слове в случайном порядке. Так же была добавлена возможность, изменения порядка в нескольких словах, засчёт внедрения цикла do{}while();.

--- changeLetterOrder/Program.cs
+++ changeLetterOrder/Program.cs
@@ -1,11 +1,49 @@
-﻿Console.Write("Введите что - нибудь(например, Ваше имя!) : ");
-string enter = Console.ReadLine();
-string result = "";
-for(int i = enter.Length - 1;i > -1; i--)
+﻿do
 {
-    result += enter[i];
-}
-Console.WriteLine(result);
-// или короче
-string shortestResult = string.Join("", enter.ToCharArray().Reverse());
-Console.WriteLine(shortestResult);
+    Console.Write("Введите что - нибудь(например, Ваше имя!) : ");
+    string enter = Console.ReadLine();
+    string result = "";
+    int chosenOrder = 0;
+    Random random = new Random();
+
+
+    Console.WriteLine("Выберете новый порядок букв во введённом слове:");
+    Console.WriteLine("1 - обратный;\n2 - случайный;");
+    int.TryParse(Console.ReadLine(), out chosenOrder);
+
+    switch (chosenOrder)
+    {
+        case 1:
+            for (int i = enter.Length - 1; i > -1; i--)
+            {
+                result += enter[i];
+            }
+            Console.WriteLine(result);
+            // или короче
+            /* string shortestResult = string.Join("", enter.ToCharArray().Reverse());
+            ** Console.WriteLine(shortestResult);*/
+            ; break;
+        case 2:
+            char temp;
+            int indexOfChar = 0;
+            int step = 0;
+            char[] enterChars = enter.ToCharArray();
+            while (step < enterChars.Length)
+            {
+                temp = enterChars[step];
+                indexOfChar = random.Next(0, enterChars.Length);
+                enterChars[step] = enterChars[indexOfChar];
+                enterChars[indexOfChar] = temp;
+                step++;
+            }
+            result = String.Concat(enterChars);
+            Console.WriteLine(result);
+            ; break;
+        default:
+            Console.WriteLine("Такой порядок ещё не придуман!");
+            ; break;
+    }
+    Console.Beep();
+    Console.WriteLine("Если хотите переставить буквы ещё в каком-нибудь слове - нажмите Enter; хотите закончить - Escape!");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
