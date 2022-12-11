890b1eae9b171c0e7e778a41870f55115e008caf
Anton Spiridonov <hoorbazoom@gmail.com>
2022-12-11 16:30:44 +0700

    Добавьте файлы проекта.

---
+++ changeLetterOrder/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите что - нибудь(например, Ваше имя!) : ");
+string enter = Console.ReadLine();
+string result = "";
+for(int i = enter.Length - 1;i > -1; i--)
+{
+    result += enter[i];
+}
+Console.WriteLine(result);
+// или короче
+string shortestResult = string.Join("", enter.ToCharArray().Reverse());
+Console.WriteLine(shortestResult);
